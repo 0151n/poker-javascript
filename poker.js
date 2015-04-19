ef2a1d10fdfd4d5ac3c568ce936ded73ea86ca6c
0151n <oisinohall@gmail.com>
2015-04-19 16:41:12 +0100

    gui layout done

--- poker.js
+++ poker.js
@@ -1,25 +1,31 @@
 //Main poker game script
 //create game instance
-var game = new Game(5);
-var test = new Player("test");
-test.populate([],52,5);
-test.get_rank();
-//print hand
-for(var j = 0;j < 5;j++){
-    var used = [];
-    game.players[j].populate(used,52,5);
-    game.players[j].get_rank();
-    document.write("<h2>" + game.players[j].name + "</h2>");
-    document.write("<hr>");
-    used = used.concat(used,game.players[j].hand);
-    for(var i = 0;i < 5;i++){
-        document.write(game.value_names[game.values[game.players[j].hand[i]]] + " " + game.suit_names[game.suits[game.players[j].hand[i]]] + "<br>");
+function run_game() {
+    var game = new Game(5);
+    var test = new Player("test");
+    test.populate([],52,5);
+    test.get_rank();
+    //print hand
+    for(var j = 0;j < 5;j++){
+        var used = [];
+        game.players[j].populate(used,52,5);
+        game.players[j].get_rank();
+        used = used.concat(used,game.players[j].hand);
     }
-    document.write("<h3>" + game.ranks[game.players[j].ranks[0]] + "</h3><br><hr>");
-}
-var winner = game.compare_players([true,true,true,true,true],0);
-if(winner > -1){
-    document.write("<h2>" + game.players[winner].name + " Has Won</h2>");
-}else if(winner == -1){
-    document.write("<h2>Game Was a tie</h2>");
+    var image;
+    for(var j = 1;j < 5;j++){
+        for(var i = 0;i < 5;i++){
+           image = document.getElementById(("c" + j) + i);
+           image.src = ("images/" + game.players[j].hand[i] + ".png");
+            //document.write((("c" + j) + i));
+        }
+        //document.write("<h3>" + game.ranks[game.players[j].ranks[0]]);
+       // document.write("<h3>" + ("c" + j) + i+ "</h3");
+    }
+    var winner = game.compare_players([true,true,true,true,true],0);
+    if(winner > -1){
+        document.getElementById("winner").value = (game.players[winner].name);
+    }else if(winner == -1){
+        document.getElementById("winner").value = ("Tie");
+    }
 }
